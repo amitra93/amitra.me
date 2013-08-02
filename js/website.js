21d2b989c85273c65d05268de9f019bf7dc3caa0
Anarghya Mitra <amitra93@gmail.com>
2013-08-01 21:50:35 -0500

    Fixed navigation menu

--- js/website.js
+++ js/website.js
@@ -1,9 +1,25 @@
+var menuItemNames = ['#background', '#about', '#projects', '#resume', '#contact'];
+
 $(document).ready(function() {
 	$('#page_holder').pagify({
 		pages: ['background', 'about', 'resume', 'projects', 'contact'],
 		animation: 'fadeIn',
 		animationOut: 'fadeOut',
 		'default': 'background',
-		cache: false
+		cache: false,
+		onChange: initialSelect
 	});
+	
+	$(menuItemNames.join(', ')).click(safeSelect);
 });
+
+function safeSelect() {  
+    var $this = $(this);
+    $this.removeClass('unselected').addClass('selected');
+    $this.closest('ul').find('a').not(this).removeClass('selected').addClass('unselected');
+}
+
+function initialSelect(pageId){
+	console.log(pageId);
+	$('#'+pageId).click();
+}
